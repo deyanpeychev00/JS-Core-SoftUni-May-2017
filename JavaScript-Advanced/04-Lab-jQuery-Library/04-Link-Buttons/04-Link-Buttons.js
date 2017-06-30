e1d5265894213440bab528619405353c383abc82
Deyan Peychev <deyan.p.peychev@gmail.com>
2017-06-30 18:38:40 +0300

    Reordered repository

--- JavaScript-Advanced/04-Lab-jQuery-Library/04-Link-Buttons/04-Link-Buttons.js
+++
@@ -1,8 +0,0 @@
-function attachEvents() {
-    $('a.button').click(modify);
-
-    function modify() {
-        $('.selected').removeClass('selected');
-        $(this).addClass('selected');
-    }
-}
